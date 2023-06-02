2a71f71037d55e8fb9cb8e318585b7e42cd279b6
WhiteBlackGoose <wbg@angouri.org>
2023-06-02 20:29:35 +0200

    parser WIP

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,18 @@ module Main where
 
 import Operations (vars, sub, alpha, beta, eta)
 import Types (VarName, Expression (Variable, Application, Abstraction))
+import Parser (pLambda)
+
+import Data.Void (Void)
+import Text.Megaparsec (Parsec)
+import Text.Megaparsec.Char (char)
+type Parser = Parsec Void String
+mySequence :: Parser (Char, Char, Char)
+mySequence = do
+  a <- char 'a'
+  b <- char 'b'
+  c <- char 'c'
+  return (a, b, c)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,2 +1,33 @@
 module Parser (
+  pLambda    
 ) where
+
+import Text.Megaparsec (Parsec, some, single)
+
+import Types (VarName, Expression (Variable, Application, Abstraction))
+import qualified Text.ParserCombinators.ReadPrec
+import Text.Read (Read(readPrec))
+import Data.Void (Void)
+import Text.Megaparsec.Byte (char, alphaNumChar)
+import Text.Megaparsec (choice)
+
+type Parser = Parsec Void String
+
+data Result =
+  Valid Expression
+  | Error
+  deriving (Show)
+
+rmap f (Valid e) = Valid (f e)
+rmap _ err = err
+
+pAlpha :: Parser VarName
+pAlpha = choice $ map single ['a'..'z']
+
+pLambda :: Parser (Expression -> Expression)
+pLambda = do
+  _ <- single '|'
+  vs <- some pAlpha
+  _ <- single '.'
+  return $ foldl (.) id (map Abstraction vs)
+
